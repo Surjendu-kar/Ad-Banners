7309ef7d72c57767ff34ab3767053e77a68fb5d9
Surjendu kar <rahulgotp9988@gmail.com>
2024-07-31 00:16:20 +0530

    Add list of banners

--- components/BannerImageComp.tsx
+++ components/BannerImageComp.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
 
 type BannerProps = {
   title: string;
@@ -6,23 +7,77 @@ type BannerProps = {
   cta: string;
   image: string;
   background: string;
+  titleColor: string;
+  ctaColor: string;
+  ctaBgColor: string;
 };
 
-const BannerImageComp: React.FC<BannerProps> = ({ title, description, cta, image, background }) => {
+const BannerImageComp: React.FC<BannerProps> = ({
+  title,
+  description,
+  cta,
+  image,
+  background,
+  titleColor,
+  ctaColor,
+  ctaBgColor,
+}) => {
   return (
-    <div style={{
-      backgroundImage: `url(${background})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      padding: '20px',
-      borderRadius: '8px',
-      textAlign: 'center'
-    }}>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <button>{cta}</button>
-      {/* <img src={image} alt={title} style={{ width: '100%', marginTop: '10px', borderRadius: '8px' }} /> */}
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        overflow: "hidden",
+        height: "300px",
+        width: "650px",
+      }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", padding: "3rem" }}>
+        <Typography
+          variant="h5"
+          component="h2"
+          gutterBottom
+          sx={{ color: titleColor }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{ color: titleColor }}
+        >
+          {description}
+        </Typography>
+        <Box>
+          <Button
+            variant="contained"
+            sx={{
+              mt: 2,
+              color: ctaColor,
+              background: ctaBgColor,
+
+              "&:hover": {
+                color: ctaBgColor,
+                background: ctaColor,
+                opacity: 0.9,
+              },
+            }}
+          >
+            {cta}
+          </Button>
+        </Box>
+      </Box>
+      <Box>
+        <img
+          src={image}
+          alt={title}
+          style={{ width: "100%", maxHeight: "140px", objectFit: "cover" }}
+        />
+      </Box>
+    </Box>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
 import banners from "../data/banners.json";
 import BannerImageComp from "../components/BannerImageComp";
+import { Box } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+      }}
+    >
       {banners.banners.map((banner, index) => (
         <BannerImageComp
           key={index}
@@ -13,9 +22,12 @@ const Home: NextPage = () => {
           cta={banner.cta}
           image={banner.image}
           background={banner.background}
+          titleColor={banner.titleColor}
+          ctaColor={banner.ctaColor}
+          ctaBgColor={banner.ctaBgColor}
         />
       ))}
-    </div>
+    </Box>
   );
 };
 
